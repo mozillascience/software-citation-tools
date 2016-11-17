eb2454acd11b656a16813a1bdbc7e6257d5d486f
Robert Lowe <rob@lowefrequency.com>
2016-11-17 00:53:59 -0500

    Set up initial bibtex misc style formatter

---
+++ core/model/formats/bibtexmisc.js
@@ -0,0 +1,19 @@
+'use strict';
+require('../../../utils/array');
+
+/**
+ * @module bibTex Misc Entry Formatter
+ * Module for formatting source data into a bibTeX misc type entry
+ */
+
+module.exports = {
+    /**
+	 * Formats a source data object to a citation
+	 * @param {SourceData} sourceData - The SourceData object to generate the citation string from
+	 * @return {string} - The bibTeX Misc Entry
+	 */
+    format : (sourceData){
+        return "";
+    }
+}
+    
